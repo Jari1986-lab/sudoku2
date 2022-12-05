97cf80d717359461b67a7001097b623d96711ebe
jari <iceman198611@outlook.com>
2022-12-05 15:39:55 +0200

    tehtävä

---
+++ sudoku_sarake.py
@@ -0,0 +1,24 @@
+def sarake_oikein(sudoku:list, sarake_nro: int):
+    test = []
+    for i in range(0, 9):
+        test.append(sudoku[i][sarake_nro])
+        for i in range(1, 10):
+            if test.count(i) > 1:
+             return False
+    return True
+
+if __name__ == "__main__":
+
+    sudoku =[
+    [9,0,0,0,8,0,3,0,0],
+    [2,0,0,2,5,0,7,0,0],
+    [0,2,0,3,0,0,0,0,4],
+    [2,9,4,0,0,0,0,0,0],
+    [0,0,0,7,3,0,5,6,0],
+    [7,0,6,0,5,0,4,0,0],
+    [0,0,7,8,0,3,9,0,0],
+    [0,1,0,0,0,0,0,0,3],
+    [3,0,0,0,0,0,0,0,2]
+    ]
+    print(sarake_oikein(sudoku, 0))
+    print(sarake_oikein(sudoku, 1))
